ca715f1d977a1cd6f88fce483df69584bf807f0f
ecorbetthp <ecorbett@hybridpathways.com>
2024-01-03 11:49:57 -0500

    ContentView updates

--- hikerlogix/ContentView.swift
+++ hikerlogix/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showDashboard = false
+<<<<<<< HEAD
     
     var body: some View {
         ZStack{
@@ -36,3 +37,62 @@ struct ContentView: View {
             DashboardView()
         }
     }
+=======
+    @State private var showContentView = false
+    
+    var body: some View {
+         ZStack{
+
+             // View Background Image
+             Image("mainScreen")
+                 .resizable()
+                 .scaledToFill()
+                 .opacity(0.3)
+                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                 .zIndex(-1)
+
+             // Main Content
+             VStack (spacing: 20){
+                 // Dashboard Link
+                 Button(action: {
+                     showDashboard = true
+                 }) {
+                     VStack {
+                         Image(systemName: "gauge.open.with.lines.needle.33percent")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                Text("Dashboard")
+                            }
+                        }
+                        .sheet(isPresented: $showDashboard) {
+                            DashboardView()
+                        }
+
+                        // Leave no trace link
+                        Button(action: {
+                            showContentView = true
+                        }) {
+                            VStack {
+                                Image(systemName: "figure.hiking")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                Text("Leave No Trace")
+                            }
+                            .sheet(isPresented: $showContentView) {
+                                ContentView()
+                            }
+                        }
+                        .padding()
+                    }
+                }
+            }
+        }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
+>>>>>>> 4aea005 (new views)
